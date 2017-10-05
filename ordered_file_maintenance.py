c53ac13bf490d052660b03753a6b3450e27f700c
Lopsy <linyks@gmail.com>
2017-10-04 21:00:52 -0400

    Update ordered_file_maintenance.py

--- ordered_file_maintenance.py
+++ ordered_file_maintenance.py
@@ -3,12 +3,11 @@
 import math
 
 
-class OrderedFile:
-    arr = []
+class OrderedFile(list):
 
     # add the element elem to the array at position pos, rewriting as needed to make space
     # return nothing; modify the array in place
-    def insert(elem, pos):
+    def insert(arr, elem, pos):
             if pos > len(arr):
                     raise ValueError("attempted inserting past the end of the array")
             
@@ -43,30 +42,30 @@ class OrderedFile:
 
     # remove the element from the array at the given position
     # return nothing; modify the array in place
-    def delete(pos):
+    def delete(arr, pos):
         pass
 
     # return the number of items between i and j-1 in the array
     # also, while scanning, rewrite all elements from i to j-1 to be on the left side of the interval
-    def _scan(i, j):
+    def _scan(arr, i, j):
             pass
 
     # given the level up the tree we're at, we have 2^level * log(n) things in a block
     # returns how many blocks there are in the tree at that level
-    def _num_blocks(level):
+    def _num_blocks(arr, level):
             # ceiling function of n/2^level*logn
             n = len(arr)
             return int((n-1)/(2**level * math.log(n, 2)))+1
 
     # rewrites all elements from i to j-1 to be evenly spread across the interval and return nothing
     # also inserts elem at the specified position while it's rewriting
-    def _rewrite(i, j, elem=None):
+    def _rewrite(arr, i, j, elem=None):
             # call _collapse and _even_spread
             pass
 
     # rewrites all elements from i to j-1 to be on the left side of the interval,
     # and returns a count of how many elements it found
-    def _collapse(self, i, j):
+    def _collapse(arr, i, j):
 	    found = [elem for elem in arr[i:j] if elem is not None]
 	    rewritten = found + [None]*(j-i-len(found))
 	    arr[i:j] = rewritten
@@ -74,7 +73,7 @@ class OrderedFile:
 
     # given the elements from i to j-1 are all on the left side of the interval, and there are n of them,
     # rewrite the elements to be evenly spread across the interval and return nothing
-    def _even_spread(i, j, n):
+    def _even_spread(arr, i, j, n):
         block_size = j - i
         interval = block_size / n
 
