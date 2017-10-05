b6d6f8db836b149d77d42097fbd73920b451a344
Jackie Bredenberg <jamb@mit.edu>
2017-10-04 20:09:48 -0400

    wrote _num_blocks

--- ordered_file_maintenance.py
+++ ordered_file_maintenance.py
@@ -10,8 +10,11 @@ def insert(arr, pos):
 def _scan(arr, i, j):
 	pass
 
+# given the level up the tree we're at, we have 2^level * log(n) things in a block
+# returns how many blocks there are in the tree at that level
 def _num_blocks(arr, level):
-	pass
+	# ceiling function of n/2^level*logn
+	return int((n-1)/(2**level * math.log(n, 2)))+1
 
 # given the elements from i to j-1 are all on the left side of the interval, and there are count of them,
 # rewrite the elements to be evenly spread across the interval and return nothing
