3afd44c8e7391c1ee3621d798d208c50338ff123
Jackie Bredenberg <jamb@mit.edu>
2017-10-04 20:03:51 -0400

    Defining functions to be implemented by our group

---
+++ ordered_file_maintenance.py
@@ -0,0 +1,29 @@
+# jamb, rusch, nchanda2 
+
+arr = []
+
+def insert(arr, pos):
+	pass
+
+# return the number of items between i and j-1 in the array
+def _scan(arr, i, j):
+	pass
+
+def _num_blocks(arr, level):
+	pass
+
+# rewrites all elements from i to j-1 to be evenly spread across the interval and return nothing
+def _rewrite(arr, i, j):
+	# call _collapse and _even_spread
+	pass
+
+# rewrites all elements from i to j-1 to be on the left side of the interval,
+# and returns a count of how many elements it found
+def _collapse(arr, i, j):
+	pass
+
+# given the elements from i to j-1 are all on the left side of the interval, and there are count of them,
+# rewrite the elements to be evenly spread across the interval and return nothing
+def _even_spread(arr, i, j, count):
+	pass
+
