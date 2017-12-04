135f11f48619a4e1c08dd25a77daa87927a9f3be
Robert C Rusch <rusch@mit.edu>
2017-12-03 21:35:33 -0500

    Clean test

--- fp_tests.py
+++ fp_tests.py
@@ -15,6 +15,18 @@ class TestFullPersistence(unittest.TestCase):
         ffpm.first_version.root = node0
         node1 = FPNode("n1", ffpm, ffpm.first_version)
         v1 = node0.set_field("p0", node1, ffpm.first_version)
+        n1 = node0.get_field("p0", v1)
+        assert(node1 == n1)
+        v2 = node1.set_field("p0", node1, ffpm.first_version)
+        n1 = node0.get_field("p0", v2)
+        assert(node1 == n1)
+
+    def test_cycle(self):
+        ffpm = FPPM()
+        node0 = FPNode("n0", ffpm, ffpm.first_version)
+        ffpm.first_version.root = node0
+        node1 = FPNode("n1", ffpm, ffpm.first_version)
+        v1 = node0.set_field("p0", node1, ffpm.first_version)
         n1 = node0.get_field("p0", ffpm.first_version)
         assert(node1 == n1)
         node1.set_field("p0", node1, ffpm.first_version)
