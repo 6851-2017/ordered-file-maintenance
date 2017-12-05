1d64d7c374b62d02232c70e7729e5406eee26cb9
Robert C Rusch <rusch@mit.edu>
2017-12-04 21:22:49 -0500

    Start on benchmarking

---
+++ benchmarking.py
@@ -0,0 +1,17 @@
+import unittest
+from full_persistence import FPPM
+from full_persistence import FPNode
+
+# Linked list
+
+# 4 depth binary history for each node
+
+
+def create_singly_linked_list(n):
+    pass
+
+def traverse_l
+    pass
+
+
+def main():
